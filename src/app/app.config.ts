c59f46b31357284b28a90e6c9bef9f5704eee750
KpSantiago <santiago.kauajose11@gmail.com>
2024-04-24 21:11:51 -0300

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,15 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(HttpClientXsrfModule.withOptions({
+      cookieName: 'sessionId',
+      headerName: 'Cookie'
+    }))
   ]
 }
